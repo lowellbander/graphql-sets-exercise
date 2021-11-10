687ad284790243350e09773eebf267f81b642e95
Lowell Bander <lowellb@fb.com>
2021-11-10 07:06:32 -0800

    speedy-ish in-memory intersectingSects implementation

--- src/database.js
+++ src/database.js
@@ -1,20 +1,23 @@
 // node-graphql/src/database.js
 
 const sets = [
-    {
-      id: 1,
-      members: new Set([1, 2, 3]),
-    },
-    {
-      id: 2,
-      members: new Set([1, 2, 3]),
-    },
-    {
-      id: 3,
-      members: new Set([1, 2, 3]),
-    },
-  ]
+    // {
+    //   id: 1,
+    //   members: new Set([1, 2, 3]),
+    // },
+    // {
+    //   id: 2,
+    //   members: new Set([1, 2, 3]),
+    // },
+    // {
+    //   id: 3,
+    //   members: new Set([1, 2, 3]),
+    // },
+  ];
 
-  module.exports = {
-    sets,
-  }
+const membersToSets = {}; // {element: [set]}
+
+module.exports = {
+	sets,
+	membersToSets,
+}
--- src/resolvers.js
+++ src/resolvers.js
@@ -1,31 +1,55 @@
 // node-graphql/src/resolvers.js
 
-const { sets } =  require('./database.js');
+const { sets, membersToSets } =  require('./database.js');
 
 const resolvers = {
+  Set: {
+      id: (parent, args, context, info) => parent.id,
+      members: (parent) => parent.members,
+      intersectingSets,
+  },
 
-    Set: {
-        id: (parent, args, context, info) => parent.id,
-        members: (parent) => parent.members,
-        intersectingSets: (parent) => {throw new Error('unimplemented')},
+  Query: {
+    sets: (parent, args) => {
+      return sets;
     },
+  },
 
-    Query: {
-      sets: (parent, args) => {
-        return sets;
-      },
-    },
+  Mutation: {
+    createSet,
+  },
+};
 
-    Mutation: {
-      createSet: (parent, args) => {
-        sets.push({
-          id: sets.length + 1,
-          members: new Set(args.input.members),
-        })
-        return sets[sets.length - 1];
-      },
-    },
+function intersectingSets(parent) {
+  const idsToReturn = new Set();
+  const setsToReturn = [];
+  const {id: thisID, members: theseMembers} = parent;
+  for (const member of theseMembers.values()) {
+    for (const candidateSet of membersToSets[member]) {
+      if (candidateSet.id === thisID || idsToReturn.has(candidateSet.id)) {
+        continue;
+      }
+      idsToReturn.add(candidateSet.id);
+      setsToReturn.push(candidateSet);
+    }
   }
+  return setsToReturn;
+}
+
+function createSet (parent, args) {
+  const newSet  = {
+    id: sets.length + 1,
+    members: new Set(args.input.members),
+  };
+  sets.push(newSet);
+  for (const member of newSet.members.values()) {
+    if (!membersToSets.hasOwnProperty(member)) {
+      membersToSets[member] = [];
+    }
+    membersToSets[member].push(newSet);
+  }
+  return newSet;
+}
 
   module.exports = {
     resolvers,
